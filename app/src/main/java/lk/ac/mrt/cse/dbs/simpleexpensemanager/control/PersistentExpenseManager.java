f9807c688ee48180535ffa4e143593c3b2f3e02b
JudeRanidu <47142837+JudeRanidu@users.noreply.github.com>
2019-12-19 19:07:38 +0530

    final

--- app/src/main/java/lk/ac/mrt/cse/dbs/simpleexpensemanager/control/PersistentExpenseManager.java
+++ app/src/main/java/lk/ac/mrt/cse/dbs/simpleexpensemanager/control/PersistentExpenseManager.java
@@ -1,5 +1,28 @@
 package lk.ac.mrt.cse.dbs.simpleexpensemanager.control;
 
-public class PersistentExpenseManager {
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
 
+import lk.ac.mrt.cse.dbs.simpleexpensemanager.control.exception.ExpenseManagerException;
+import lk.ac.mrt.cse.dbs.simpleexpensemanager.data.AccountDAO;
+import lk.ac.mrt.cse.dbs.simpleexpensemanager.data.TransactionDAO;
+import lk.ac.mrt.cse.dbs.simpleexpensemanager.data.database.ExpenseManagerDBHelper;
+import lk.ac.mrt.cse.dbs.simpleexpensemanager.data.impl.PersistentAccountDAO;
+import lk.ac.mrt.cse.dbs.simpleexpensemanager.data.impl.PersistentTransactionDAO;
+
+public class PersistentExpenseManager extends ExpenseManager{
+    private transient Context context;
+    public PersistentExpenseManager(Context context)throws ExpenseManagerException{
+        this.context=context;
+        setup();
+    }
+    @Override
+    public void setup() throws ExpenseManagerException {
+        SQLiteDatabase database = new ExpenseManagerDBHelper(this.context).getWritableDatabase();
+        TransactionDAO persistentTransactionDAO = new PersistentTransactionDAO(database);
+        setTransactionsDAO(persistentTransactionDAO);
+
+        AccountDAO persistentAccountDAO = new PersistentAccountDAO(database);
+        setAccountsDAO(persistentAccountDAO);
+    }
 }
